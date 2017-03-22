d8871fd7bea931f98efdc69a84f6b2ad2872761a
Stefan Seegel <post@seegel-systeme.de>
2017-03-23 00:01:00 +0100

    fix AB440S double entry in device dict

--- raspyrfm_client/device/manufacturer/elro/AB440ID.py
+++ raspyrfm_client/device/manufacturer/elro/AB440ID.py
@@ -7,10 +7,9 @@ class AB440ID(AB440S):
 
     _dips = ['1', '2', '3', '4', '5', '6', '7', '8']
 
-    def __init__(self):
-        from raspyrfm_client.device.manufacturer import manufacturer_constants
-        super(AB440S, self).__init__(manufacturer_constants.ELRO,
-                                     manufacturer_constants.AB440ID)
+    from raspyrfm_client.device.manufacturer import manufacturer_constants
+    def __init__(self, manufacturer: str = manufacturer_constants.ELRO, model: str = manufacturer_constants.AB440ID):
+        super(AB440S, self).__init__(manufacturer, model)
 
     def generate_code(self, action: str) -> str:
         dips = self.get_channel_config()
--- raspyrfm_client/device/manufacturer/elro/AB440IS.py
+++ raspyrfm_client/device/manufacturer/elro/AB440IS.py
@@ -5,7 +5,7 @@ from raspyrfm_client.device.manufacturer.elro.AB440ID import AB440ID
 class AB440IS(AB440ID):
     _dips = ['1', '2', '3', '4', '5', 'A', 'B', 'C']
 
-    _model = manufacturer_constants.AB440IS
-
     def __init__(self):
-        super(AB440ID, self).__init__()
+        from raspyrfm_client.device.manufacturer import manufacturer_constants
+        super().__init__(manufacturer_constants.ELRO,
+                         manufacturer_constants.AB440ID)
