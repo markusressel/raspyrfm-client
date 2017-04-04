398f326641bd395ecc925fc552b727e6873c879f
Markus Ressel <markus9100@gmail.com>
2017-04-04 19:11:25 +0200

    v1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='raspyrfm_client',
-    version='1.0.0',
+    version='1.1.0',
     description='A library to send rc signals with the RaspyRFM module',
     license='GPLv3+',
     author='Markus Ressel',
