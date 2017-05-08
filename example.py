cd29c0566fb81273b9713681e10f50d7ef57f56e
Markus Ressel <markus9100@gmail.com>
2017-05-08 04:16:37 +0200

    fixed example.py

--- example.py
+++ example.py
@@ -1,24 +1,27 @@
 from raspyrfm_client import RaspyRFMClient
-from raspyrfm_client.device_implementations.controlunit import controlunit_constants, actions
+from raspyrfm_client.device_implementations.controlunit.actions import Action
+from raspyrfm_client.device_implementations.controlunit.controlunit_constants import ControlUnitModel
+from raspyrfm_client.device_implementations.gateway.manufacturer.gateway_constants import GatewayModel
+from raspyrfm_client.device_implementations.manufacturer_constants import Manufacturer
 
 """ RaspyRFM Client """
-rfm_client = RaspyRFMClient("192.168.2.10")
-# rfm_client = RaspyRFMClient()
-# print(rfm_client.search())
+rfm_client = RaspyRFMClient()
 
 rfm_client.list_supported_controlunits()
 
-print("Host: " + str(rfm_client.get_host()))
-print("Port: " + str(rfm_client.get_port()))
-print("Manufacturer: " + str(rfm_client.get_manufacturer()))
-print("Model: " + str(rfm_client.get_model()))
-print("Firmware: " + str(rfm_client.get_firmware_version()))
+raspyrfm = rfm_client.get_gateway(Manufacturer.SEEGEL_SYSTEME, GatewayModel.RASPYRFM, "192.168.2.10")
+
+print("Host: " + str(raspyrfm.get_host()))
+print("Port: " + str(raspyrfm.get_port()))
+print("Manufacturer: " + str(raspyrfm.get_manufacturer()))
+print("Model: " + str(raspyrfm.get_model()))
+print("Firmware: " + str(raspyrfm.get_firmware_version()))
 
 """ Brennenstuhl """
 print("")
 
-brennenstuhl_rcs1000 = rfm_client.get_controlunit(controlunit_constants.BRENNENSTUHL,
-                                                  controlunit_constants.RCS_1000_N_COMFORT)
+brennenstuhl_rcs1000 = rfm_client.get_controlunit(Manufacturer.BRENNENSTUHL,
+                                                  ControlUnitModel.RCS_1000_N_COMFORT)
 brennenstuhl_rcs1000.set_channel_config(**{
     '1': 1,
     '2': 1,
@@ -30,14 +33,14 @@ brennenstuhl_rcs1000.set_channel_config(**{
 
 print(str(brennenstuhl_rcs1000))
 
-print(brennenstuhl_rcs1000.generate_code(actions.ON))
-print(brennenstuhl_rcs1000.generate_code(actions.OFF))
-rfm_client.send(brennenstuhl_rcs1000, actions.ON)
+print(raspyrfm.generate_code(brennenstuhl_rcs1000, Action.ON))
+print(raspyrfm.generate_code(brennenstuhl_rcs1000, Action.OFF))
+rfm_client.send(raspyrfm, brennenstuhl_rcs1000, Action.ON)
 
 """ Elro """
 print("")
 
-elro_ab440id = rfm_client.get_controlunit(controlunit_constants.ELRO, controlunit_constants.AB440ID)
+elro_ab440id = rfm_client.get_controlunit(Manufacturer.ELRO, ControlUnitModel.AB440ID)
 
 elro_ab440id.set_channel_config(**{
     '1': 0,
@@ -50,11 +53,11 @@ elro_ab440id.set_channel_config(**{
 
 print(str(elro_ab440id))
 
-print(elro_ab440id.generate_code(actions.ON))
-print(elro_ab440id.generate_code(actions.OFF))
+print(raspyrfm.generate_code(elro_ab440id, Action.ON))
+print(raspyrfm.generate_code(elro_ab440id, Action.OFF))
 
 print("")
-elro_ab440s = rfm_client.get_controlunit(controlunit_constants.ELRO, controlunit_constants.AB440S)
+elro_ab440s = rfm_client.get_controlunit(Manufacturer.ELRO, ControlUnitModel.AB440S)
 
 elro_ab440s.set_channel_config(**{
     '1': 0,
@@ -67,26 +70,26 @@ elro_ab440s.set_channel_config(**{
 
 print(str(elro_ab440s))
 
-print(elro_ab440s.generate_code(actions.ON))
-print(elro_ab440s.generate_code(actions.OFF))
+print(raspyrfm.generate_code(elro_ab440s, Action.ON))
+print(raspyrfm.generate_code(elro_ab440s, Action.OFF))
 
 """ rev """
 print("")
 
-rev_telecontrol = rfm_client.get_controlunit(controlunit_constants.REV, controlunit_constants.TELECONTROL8342C)
+rev_telecontrol = rfm_client.get_controlunit(Manufacturer.REV, ControlUnitModel.TELECONTROL8342C)
 
 rev_telecontrol.set_channel_config(master='A', slave=1)
 
 print(str(rev_telecontrol))
 
-print(rev_telecontrol.generate_code(actions.ON))
-print(rev_telecontrol.generate_code(actions.OFF))
+print(raspyrfm.generate_code(rev_telecontrol, Action.ON))
+print(raspyrfm.generate_code(rev_telecontrol, Action.OFF))
 
-rfm_client.send(rev_telecontrol, actions.ON)
+rfm_client.send(raspyrfm, rev_telecontrol, Action.ON)
 
 print("")
 
-rev_ritter = rfm_client.get_controlunit(controlunit_constants.REV, controlunit_constants.RITTER)
+rev_ritter = rfm_client.get_controlunit(Manufacturer.REV, ControlUnitModel.RITTER)
 
 rev_ritter.set_channel_config(**{
     '1': 0,
@@ -100,38 +103,39 @@ rev_ritter.set_channel_config(**{
 
 print(str(rev_ritter))
 
-print(rev_ritter.generate_code(actions.ON))
+print(raspyrfm.generate_code(rev_ritter, Action.ON))
 
 """ intertechno """
 print("")
 
-intertechno_cmr_1000 = rfm_client.get_controlunit(controlunit_constants.INTERTECHNO, controlunit_constants.CMR_1000)
+intertechno_cmr_1000 = rfm_client.get_controlunit(Manufacturer.INTERTECHNO, ControlUnitModel.CMR_1000)
 
 intertechno_cmr_1000.set_channel_config(master='A', slave=1)
 
 print(str(intertechno_cmr_1000))
 
-print(intertechno_cmr_1000.generate_code(actions.ON))
-print(intertechno_cmr_1000.generate_code(actions.OFF))
+print(raspyrfm.generate_code(intertechno_cmr_1000, Action.ON))
+print(raspyrfm.generate_code(intertechno_cmr_1000, Action.OFF))
 
 intertechno_cmr_1000.set_channel_config(master='P', slave=16)
-print(intertechno_cmr_1000.generate_code(actions.ON))
-print(intertechno_cmr_1000.generate_code(actions.OFF))
+
+print(raspyrfm.generate_code(intertechno_cmr_1000, Action.ON))
+print(raspyrfm.generate_code(intertechno_cmr_1000, Action.OFF))
 
 print("")
 
-intertechno_cmr_500 = rfm_client.get_controlunit(controlunit_constants.INTERTECHNO, controlunit_constants.CMR_500)
+intertechno_cmr_500 = rfm_client.get_controlunit(Manufacturer.INTERTECHNO, ControlUnitModel.CMR_500)
 
 intertechno_cmr_500.set_channel_config(master='A', slave=1)
 
 print(str(intertechno_cmr_500))
 
-print(intertechno_cmr_500.generate_code(actions.ON))
-print(intertechno_cmr_500.generate_code(actions.OFF))
+print(raspyrfm.generate_code(intertechno_cmr_500, Action.ON))
+print(raspyrfm.generate_code(intertechno_cmr_500, Action.OFF))
 
 """ Intertek """
 print("")
-intertek = rfm_client.get_controlunit(controlunit_constants.INTERTEK, controlunit_constants.MODEL_1919361)
+intertek = rfm_client.get_controlunit(Manufacturer.INTERTEK, ControlUnitModel.MODEL_1919361)
 
 intertek.set_channel_config(**{
     '1': 0,
@@ -144,12 +148,12 @@ intertek.set_channel_config(**{
 
 print(str(intertek))
 
-print(intertek.generate_code(actions.ON))
-print(intertek.generate_code(actions.OFF))
+print(raspyrfm.generate_code(intertek, Action.ON))
+print(raspyrfm.generate_code(intertek, Action.OFF))
 
 """ Intertechno ITS-150 """
 print("")
-its150 = rfm_client.get_controlunit(controlunit_constants.INTERTECHNO, controlunit_constants.ITS_150)
+its150 = rfm_client.get_controlunit(Manufacturer.INTERTECHNO, ControlUnitModel.ITS_150)
 
 its150.set_channel_config(**{
     'CODE': 'I',  # house code A-P
@@ -158,4 +162,4 @@ its150.set_channel_config(**{
 })
 
 print(str(its150))
-rfm_client.send(its150, actions.ON)
+rfm_client.send(raspyrfm, its150, Action.ON)
